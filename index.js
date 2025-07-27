36ff3f093eb3b0a03c3134ce6cc5c01903d6f110
tsun <dreaminghk@gmail.com>
2025-07-27 07:35:23 +0100

    add logging

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
+
+console.log("star launch");
 // Launch the browser and open a new blank page
 const browser = await puppeteer.launch();
+
+console.log("start new page");
 const page = await browser.newPage();
 
+console.log("start goto");
 // Navigate the page to a URL.
 await page.goto('https://www.tradingview.com/symbols/NASDAQ-NVDA/');
 
+
+console.log("start close");
 await browser.close();
+console.log("close");
